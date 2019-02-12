b4e5dbba7559c476186345ffa021ddd7cf660df4
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-02-12 10:17:26 -0500

    exceptions, field

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,6 +1,7 @@
 public class KnightBoard{
+  private int[][] pubBoard;
   public KnightBoard(int startingRows,int startingCols){
-
+    if(startingRows <= 0 || startingCols <= 0) throw new IllegalArgumentException("negative row/col");
   }
 
   public String toString(){
@@ -8,10 +9,16 @@ public class KnightBoard{
   }
 
   public boolean solve(int startingRow,int startingCol){
+    if(startingRow < 0 || startingCol < 0 ||
+       startingRow >= pubBoard.length || startingCol >= pubBoard[0].length)
+       throw new IllegalArgumentException("row/col out of bounds");
     return false;
   }
 
   public int countSolutions(int startingRow,int startingCol){
+    if(startingRow < 0 || startingCol < 0 ||
+       startingRow >= pubBoard.length || startingCol >= pubBoard[0].length)
+       throw new IllegalArgumentException("row/col out of bounds");
     return -1;
   }
 }
