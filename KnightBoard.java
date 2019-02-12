9a6fcbc69c703b823cb0c004a77c97fa59bfeff4
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-02-12 10:12:27 -0500

    public method headers

---
+++ KnightBoard.java
@@ -0,0 +1,17 @@
+public class KnightBoard{
+  public KnightBoard(int startingRows,int startingCols){
+
+  }
+
+  public String toString(){
+    return "";
+  }
+
+  public boolean solve(int startingRow,int startingCol){
+    return false;
+  }
+
+  public int countSolutions(int startingRow,int startingCol){
+    return -1;
+  }
+}
