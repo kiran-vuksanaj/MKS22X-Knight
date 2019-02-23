43c8b5f8fad2206e834e956ac42df086370d9360
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-02-22 19:50:05 -0500

    method headers for space, implementation of comparable

--- Space.java
+++ Space.java
@@ -1,7 +1,43 @@
-public class Space{
+public class Space implements Comparable<Space>{
   private int step;
   private int availSteps;
   private Space[][] boardSpace;
   private int row;
   private int col;
+
+  public Space(Space[][] board,int row,int col){
+
+  }
+
+  public String toString(){
+    return "";
+  }
+
+  public int checkAvailable(){
+    return -1;
+  }
+
+  public boolean isFree(){
+    return false;
+  }
+
+  public void fill(){
+
+  }
+
+  public void empty(){
+
+  }
+
+  public Space[] getNextMoves(){
+    return null;
+  }
+
+  public int getAvailableSteps(){
+    return -1;
+  }
+
+  public int compareTo(Space other){
+    return -1;
+  }
 }
